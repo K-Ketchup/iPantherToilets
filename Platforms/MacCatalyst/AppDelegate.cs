e77c060f6c9145fc94abd230616300984cb602e0
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-05-11 23:43:27 -0700

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace iPantherToilets;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
