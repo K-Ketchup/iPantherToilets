1657cfafeaaf09beaf54595c985aa10f107d55cb
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-05-17 10:00:45 -0700

    Some changes

---
+++ FlyoutPrefsDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace iPantherToilets
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlyoutPrefsDetail : ContentPage
+    {
+        public FlyoutPrefsDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
