7e5cb3da19770d83fafe0a3ae3ac86bfaa49df0e
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-05-22 01:48:35 -0700

    Some Changes

--- InfoPage.xaml.cs
+++ InfoPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class InfoPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ReturnToHomePageButton_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushModalAsync(new MainPage());
+    }
 }
