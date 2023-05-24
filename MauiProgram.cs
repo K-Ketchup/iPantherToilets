56fbd05cc295563b9e88d8b1222197d00b9904b0
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-05-24 01:05:46 -0700

    Some changes.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("materialdesignicons-webfont.ttf", "MaterialDesignIcons");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
