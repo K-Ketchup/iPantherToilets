ee608978f58dead0e29fdb907e6d80cd516f5149
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-05-31 08:50:04 -0700

    SQLite changes.

---
+++ Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iPantherToilets.Models
+{
+    public class Review
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public int StockId { get; set; }
+        public DateTime Time { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/ReviewServices.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iPantherToilets.Services
+{
+    public static class ReviewServices
+    {
+        static async Task Init()
+        {
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "MyData.db");
+
+            var db = new SQLiteConnection(databasePath);
+        }
+
+        public static async Task AddReview(int stars)
+        {
+
+        }
+
+        public static async Task RemoveReview(int id)
+        {
+
+        }
+
+        public static async Task GetReview()
+        {
+
+        }
+
+    }
+}
