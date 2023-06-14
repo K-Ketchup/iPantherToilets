0f480c8b56d8863842bb2f90659342e9bba5213e
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-06-14 00:32:35 -0700

    Updated + added Nugets

--- ViewModel/BaseViewModel.cs
+++ ViewModel/BaseViewModel.cs
@@ -5,48 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MvvmHelpers;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace iPantherToilets.ViewModel
 {
-    sealed class CallerMemberNameAttribute : Attribute { }
-    class BaseViewModel : INotifyPropertyChanged
+    [INotifyPropertyChanged]
+    public partial class BaseViewModel
     {
         bool isBusy;
         string title;
-
-        public bool IsBusy
-        {
-            get => isBusy;
-            set
-            {
-                if (isBusy == value)
-                    return;
-
-                isBusy = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(IsNotBusy));
-            }
-        }
-
-        public string Title
-        {
-            get => title;
-            set
-            {
-                if (title == value)
-                    return;
-                title = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public bool IsNotBusy => !isBusy;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void OnPropertyChanged([CallerMemberName]string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
     }
 }
