d17e331413628ad878cc11b9bfcd5b124d8a04cd
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-06-20 03:54:02 -0700

    XAML Changes

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,8 +6,8 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-        //MainPage = new LoginPage();
-        MainPage = new MainPage();
+        MainPage = new LoginPage();
+        //MainPage = new MainPage();
         UserAppTheme = AppTheme.Light;
     }
 }
