84d29947dfcf93a95156097ead75456aec944924
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-06-27 23:50:57 -0700

    working on login

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace iPantherToilets.Models
 {
     public class UserInfo
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Passowrd { get; set; }
