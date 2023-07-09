4d6bc1413ac9ccc8cc331079327130a51fcf9ef5
K_Ketchup <121308517+K-Ketchup@users.noreply.github.com>
2023-07-08 19:08:24 -0700

    Auth0 Cross Platform Implementation

---
+++ obj/Debug/net7.0-android/android/src/crc64a7bf252cff68a046/WebAuthenticationCallbackActivity.java
@@ -0,0 +1,39 @@
+package crc64a7bf252cff68a046;
+
+
+public class WebAuthenticationCallbackActivity
+	extends crc6468b6408a11370c2f.WebAuthenticatorCallbackActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("iPantherToilets.WebAuthenticationCallbackActivity, iPantherToilets", WebAuthenticationCallbackActivity.class, __md_methods);
+	}
+
+
+	public WebAuthenticationCallbackActivity ()
+	{
+		super ();
+		if (getClass () == WebAuthenticationCallbackActivity.class) {
+			mono.android.TypeManager.Activate ("iPantherToilets.WebAuthenticationCallbackActivity, iPantherToilets", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
